82f60338800d9aaa2f9077d315176a4b62572248
HsBakir <94084650+DEVBakir@users.noreply.github.com>
2024-05-04 15:36:46 +0100

    last update

--- src/context/Auth.jsx
+++ src/context/Auth.jsx
@@ -1,22 +1,31 @@
 import { createContext, useState } from "react"
+import { Navigate, useLocation, useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext(null);
 export const ContextProvider = ({ children }) => {
+    const { pathname } = useLocation();
+    const navigate = useNavigate();
     const [token, setToken] = useState(localStorage.getItem('jwt-token'));
-    const login = (access_token,refresh_token) => {
-        localStorage.setItem('jwt-token-access', access_token);
-        localStorage.setItem('jwt-token-refresh', refresh_token);
-        setToken(access_token);
+    const [user, setUser] = useState(null);
+    const login = (data) => {
+        localStorage.setItem('jwt-token-access', data.access_token);
+        localStorage.setItem('jwt-token-refresh', data.refresh_token);
+        setUser({ fullname: data.full_name, email: data.username });
+        setToken(data.access_token);
     }
     const logout = () => {
         setToken(null)
+        setUser(null)
     }
     const showToken = () => {
         return(token);
     }
+    if (user && (pathname.toLowerCase().startsWith("/login") || pathname.toLowerCase().startsWith("/signup"))) {
+        return <Navigate to={"/"} />;
+    }
     return (
-        <AuthContext.Provider value={{login,logout, showToken}}>
+        <AuthContext.Provider value={{login,logout, showToken , user}}>
             {children}
         </AuthContext.Provider>
-   )
+   )%
 }
