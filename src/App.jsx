83f955f3732d74194f524086b20558f9395ba970
lokmantellai <105990830+lokmantellai@users.noreply.github.com>
2024-05-19 15:58:46 +0100

    "AddCourse step 1 Done !"

--- src/App.jsx
+++ src/App.jsx
@@ -22,40 +22,45 @@ import { Outlet } from 'react-router-dom/dist';
 import { useAuth } from './context/hooks';
 import { useState } from 'react';
 import ManageUsers from './pages/Admin/ManageUsers';
+import SpecialistDashboard from './pages/Specialist/SpecialistDashboard';
 
 
 
 
 function App() {
-  const { role } = useAuth(); 
+  const { role } = useAuth();
   // Check if the browser is Firefox
   var isFirefox = typeof InstallTrigger !== 'undefined';
   if (isFirefox)
     document.documentElement.style.fontSize = "14px";
   return (
     <>
-        <AuthRedirectHandler>
-          <Routes>
-            { 
+      <AuthRedirectHandler>
+        <Routes>
+          {
             <Route path='/dashboard' element={<DashboardLayout />}>
-                {role == "admin" && <Route path='users' element={<ManageUsers />}/>}
-                {role == "specialist" && <Route path='courses' element={<ManageCourses />}/>}
-                <Route path='notifactions' element={<>Notification</>} />
-                <Route path='setting' element={<>Setting</>} />
-              </Route>
-            }
-            <Route path='/test' element={<Test />} />
-            <Route path='/' element={<Home />} />
-            <Route path='/login' element={<Login />} />
-            <Route path='/login/forget-password' element={<ForgetPassword />} />
-            <Route path="/password_reset/:uidb64/:token" element={<ResetPassword />} />
-            <Route path='/signup' element={<RegisterContextProvider><SignUp /></RegisterContextProvider>} />
-            <Route path='/signup/step/:num' element={<RegisterContextProvider><SignUpStep /></RegisterContextProvider>} />
-            <Route path='/emailverification' element={<RegisterContextProvider><EmailVerification /></RegisterContextProvider>} />
-            <Route path='/profile/:id' element={<Profile />} />
-            <Route path='*' element={<NotFound />} /> {/* Catch all other routes */}
-          </Routes>
-        </AuthRedirectHandler>
+              {role == "specialist" && <Route path='' element={<SpecialistDashboard />} />}
+
+              {role == "admin" && <Route path='users' element={<ManageUsers />} />}
+              {role == "specialist" && <Route path='courses' element={<ManageCourses />} />}
+              <Route path='notifactions' element={<>Notification</>} />
+              <Route path='setting' element={<>Setting</>} />
+            </Route>
+          }
+
+
+          <Route path='/test' element={<Test />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/login/forget-password' element={<ForgetPassword />} />
+          <Route path="/password_reset/:uidb64/:token" element={<ResetPassword />} />
+          <Route path='/signup' element={<RegisterContextProvider><SignUp /></RegisterContextProvider>} />
+          <Route path='/signup/step/:num' element={<RegisterContextProvider><SignUpStep /></RegisterContextProvider>} />
+          <Route path='/emailverification' element={<RegisterContextProvider><EmailVerification /></RegisterContextProvider>} />
+          <Route path='/profile/:id' element={<Profile />} />
+          <Route path='*' element={<NotFound />} /> {/* Catch all other routes */}
+        </Routes>
+      </AuthRedirectHandler>
     </>
   )
 }
@@ -65,30 +70,72 @@ export default App;
 
 function DashboardLayout() {
   return (
-  <div className="flex  bg-[#FFFFFC]" >
+    <div className="flex  bg-[#FFFFFC]" >
       <SideBar />
       <div className=" flex flex-col flex-1 pb-[20] ">
         <Header />
         <Outlet />
       </div>
-  </div>
+    </div>
   )
 }
 
 function Test() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [choosed, setChoosed] = useState("None")
-  function click() {
-    console.log("Hey dddccq");
-    console.log("Hey dddccq");
-    console.log("Hey dddccq");
-    console.log("Hey dddccq");
+  const [imagePreview, setImagePreview] = useState(null);
+
+  const [selectedImage, setSelectedImage] = useState(null)
+
+  const handleFileChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+
+
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+
   }
   return (
-   <div>
-      <label>Test</label>
-      <br/>
-      <button onClick={click()} >Click </button>
+    <div className='flex justify-between w-[100%]'>
+      <div>
+        <input className=' bg-black' accept="image/*" type="file" onChange={handleImageChange} />
+        <div> {selectedImage && <img src={selectedImage} alt="" />}</div>
+      </div>
+
+
+      <div className="col-span-3 sm:col-span-4 my-2 flex items-center gap-10">
+        <div>
+          <input
+            id="files"
+            type="file"
+            className='hidden'
+            onChange={handleFileChange}
+            accept="image/*" // accept only image files
+          />
+          <label htmlFor="files" className="inline-flex items-center bg-[#FCEE65] hover:bg-[#FCE932] text-[#3D3700] focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-base px-4 py-2.5 text-center cursor-pointer">Upload </label>
+        </div>
+        {imagePreview ? (
+          <div className="">
+            <img src={imagePreview} alt="Preview" className="w-14 h-14 rounded-full" />
+          </div>)
+          : <span>No image selected for profile</span>
+        }
+      </div>
     </div>
+
   )
 }
