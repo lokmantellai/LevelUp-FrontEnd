e54a78acc3fd939c09a7504f8ae1ad21686bb784
lokmantellai <105990830+lokmantellai@users.noreply.github.com>
2024-05-23 06:14:36 +0100

    "last"

--- src/components/SpecialistDashboard/Newcourse.jsx
+++ src/components/SpecialistDashboard/Newcourse.jsx
@@ -42,7 +42,6 @@ export default function Newcourse({ data }) {
 
 
     const [step, setStep] = useState(1);
-    const [formData, setFormData] = useState({ title: '', img: '', description: '', degree: '', level: '', category: '' });
     const [errors, setErrors] = useState({});
     const [courseTitles, setCourseTitles] = useState([]);
 
