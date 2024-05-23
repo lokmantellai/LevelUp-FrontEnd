0cea09d46d747a442e71b738f4b7a39343ff1b1d
Oussama-kh <93738555+oussama22kh@users.noreply.github.com>
2024-05-23 01:06:57 +0100

    report page

--- src/components/SpecialistDashboard/Cours.jsx
+++ src/components/SpecialistDashboard/Cours.jsx
@@ -17,15 +17,17 @@ export default function Cours({ data, index, onClick }) {
 
 
     const click = () => {
-        onClick(data);
+        onClick(data); // Call the onClick function passed from CourseList with the course data
     }
 
 
 
     return (
+
+
         < button onClick={click} style={{ backgroundColor: bgcolor, }} className={`courseRow' w-[100%] grid grid-cols-9 gap-5    h-[70px] py-[5px] items-center text-start `} onMouseEnter={(e) => { e.currentTarget.style.backgroundColor = hvrcolor }} onMouseLeave={(e) => { e.currentTarget.style.backgroundColor = bgcolor }}>
             <div className="px-[30px] flex  items-center">
-                <img src={baseURL + '/' + data?.img_url} alt="" className="col-span-1 w-[40px] " />
+                <img src={baseURL + data?.img_url} alt="" className="col-span-1 w-[40px] " />
             </div>
             <h1 className="text-[16px] col-span-2 ">{data?.title}</h1>
             <h1 className="text-[16px] col-span-2">{data?.id}</h1>
